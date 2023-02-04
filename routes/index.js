24f585747db76ace7f1273b8d0a66a8984b55e70
fakhryhizballah <falehry88@gmail.com>
2023-02-04 09:25:31 +0700

    middelware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const storage = require('../middlewares/storage');
+const auth = require('../middlewares/');
 const media = require('../controllers/mediaController');
 const webpController = require('../controllers/webpController');
 
-router.post('/upload/img', storage.single('image'), webpController.webp, media.single);
+router.post('/upload/img', auth.check, storage.single('image'), webpController.webp, media.single);
 // router.post('/upload/multi', storage.array('image', 10), media.multi);
 module.exports = router;
