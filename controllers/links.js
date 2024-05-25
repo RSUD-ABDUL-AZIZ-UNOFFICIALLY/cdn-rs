04088626d44d5b7418cda2beaaea0452c412bbc4
fakhryhizballah <falehry88@gmail.com>
2024-05-25 17:02:19 +0700

    feat: Add short URL endpoint for creating and retrieving short links

---
+++ controllers/links.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const http = process.env.URL_STATE || "http";
+const fs = require('fs');
+module.exports = {
+    short: async (req, res) => {
+        try {
+            const Url = http + '://' + req.get('host');
+            const { author, link, newlink, description } = req.query;
+            const shortUrl = Url + '/s/' + newlink;
+            const data = {
+                author: author,
+                link: link,
+                newlink: newlink,
+                description: description,
+                shortUrl: shortUrl
+            }
+            let isExist = fs.existsSync(`./jsonDB/links/${author}.json`);
+            console.log(isExist);
+            if (!isExist) {
+                fs.writeFileSync(`./jsonDB/links/${author}.json`, JSON.stringify([]), 'utf8');
+            }
+            let cekFile = JSON.parse(fs.readFileSync(`./jsonDB/links/${author}.json`, 'utf8'));
+            cekFile.push(data);
+
+
+            // console.log(gfs);
+            res.status(200).json({
+                status: true,
+                message: "success",
+                data: {
+                    Url: Url,
+                    shortUrl: shortUrl,
+                    // cekFile: cekFile,
+                    // short: shortUrl,
+                    // url: req.body.url,
+                }
+            });
+        } catch (err) {
+            return res.status(400).json({
+                status: false,
+                message: err.message,
+                data: null
+            });
+        }
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const { upload, upload_file, upload_video } = require('../middlewares/storage');
 const auth = require('../middlewares/');
 const media = require('../controllers/mediaController');
 const webpController = require('../controllers/webpController');
+const links = require('../controllers/links');
 
 // router.post('/upload/img', auth.check, storage.single('image'), webpController.webp, media.single);
 router.post('/upload/img', upload.single('image'), webpController.webp, media.single);
@@ -13,4 +14,9 @@ router.get('/media/patch', media.patch);
 router.get('/media/fs/:id', auth.check, media.getDataFs);
 router.delete('/media/fs/:id', auth.check, media.deleteFs);
 // router.post('/upload/multi', storage.array('image', 10), media.multi);
+
+router.get('/short', links.short);
+// router.get('/s/:id', links.getShort);
+
+
 module.exports = router;
