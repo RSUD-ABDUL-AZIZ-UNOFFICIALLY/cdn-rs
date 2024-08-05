ffa467ed5999204dc76196829db3cf056caa5d98
fakhryhizballah <falehry88@gmail.com>
2024-08-05 12:34:14 +0700

    path public

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.use(express.json());
 app.use(cors())
 app.enable('trust proxy');
 
-app.use("/api/cdn/image/", express.static(path.join(__dirname + "public/imagekit")));
-app.use("/api/cdn/file/", express.static(path.join(__dirname + "public/filekit"), {
+app.use("/api/cdn/image/", express.static(path.join(__dirname + "/public/imagekit/")));
+app.use("/api/cdn/file/", express.static(path.join(__dirname + "/public/filekit/"), {
     setHeaders: (res, path, stat) => {
         res.set('Cache-Control', 'public, max-age=120');
     }
 }));
-app.use("/api/cdn/video/", express.static(path.join(__dirname + "public/video"), {
+app.use("/api/cdn/video/", express.static(path.join(__dirname + "/public/video/"), {
     setHeaders: (res, path, stat) => {
         res.set('Cache-Control', 'public, max-age=180');
     }
